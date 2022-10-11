c27055c00a498ba31499fa6822fa0696e9f40888
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-10-10 22:43:50 -0300

    -

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="di">
+                <img src="/images/logo.svg" alt="ig.news" />
+            </div>e
+        </header>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Header from "./Header";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ignews</title>
+      </Head>
+      <Header/>
+    </>
+  )
+}
