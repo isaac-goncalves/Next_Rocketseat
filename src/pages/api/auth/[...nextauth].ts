b41eb94ab937d3fe3821a1f7e7d061f550949066
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2022-10-21 12:38:43 -0300

    -

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,11 +4,12 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-        scope: "read:user"
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
 }
 export default NextAuth(authOptions)
+
+
